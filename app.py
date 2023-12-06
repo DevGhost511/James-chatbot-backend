781dd3398f250bda5e7f1a10319e5db99ad6c973
DevGhost511 <rosemax994@gmail.com>
2023-12-06 19:04:39 +0100

    "reverse cors"

--- app.py
+++ app.py
@@ -5,10 +5,10 @@ from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from utils import get_response
 from utils import generate_kb
-from flask_cors import CORS
+# from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
+# CORS(app)
 
 load_dotenv()
 
